4c96f0a9b2846b1eb5cd7cd35d7a98c7dc9ae39d
Serikzhan <serikzhan.aubakirov@gmail.com>
2023-03-26 17:07:32 +0600

    initial commit

---
+++ MVCPattern/Sources/Scenes/Model/Onboarding.swift
@@ -0,0 +1,15 @@
+//
+//  Onboarding.swift
+//  MVCPattern
+//
+//  Created by Serikzhan on 26.03.2023.
+//
+
+import Foundation
+
+struct Onboarding {
+    let index: Int
+    let title: String
+    let description: String
+    let imageName: String
+}
